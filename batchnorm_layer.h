dc50c0cd8a51fd30eefd18b71474adafdd667280
Dharani_P <144601763+Srihanu1999@users.noreply.github.com>
2025-04-15 17:43:40 +0530

    Update batchnorm_layer.h

--- batchnorm_layer.h
+++ batchnorm_layer.h
@@ -1,8 +1,7 @@
 #ifndef BATCHNORM_LAYER_H
 #define BATCHNORM_LAYER_H
 
-// Forward declaration of batchnorm forward pass
-void batchnorm_forward(float* input, float* mean, float* var,
+void batchnorm_forward(float* input, float* output, float* mean, float* var,
                        float* gamma, float* beta,
                        int N, int C, int H, int W, float epsilon);
 
