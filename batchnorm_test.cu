9b9779b2bbfb235afea5254b430bcee9dd1e0257
Dharani_P <144601763+Srihanu1999@users.noreply.github.com>
2025-04-15 17:44:30 +0530

    Update batchnorm_test.cu

--- batchnorm_test.cu
+++ batchnorm_test.cu
@@ -2,13 +2,12 @@
 #include <cuda_runtime.h>
 #include "batchnorm_layer.h"
 
-#define CHECK_CUDA(ans) { gpuAssert((ans), _FILE, __LINE_); }
+#define CHECK_CUDA(ans) { gpuAssert((ans), __FILE__, __LINE__); }
 inline void gpuAssert(cudaError_t code, const char* file, int line) {
     if (code != cudaSuccess)
         std::cerr << "CUDA Error: " << cudaGetErrorString(code) << " " << file << ":" << line << std::endl;
 }
 
-// Simple helper to print a tensor
 void print_tensor(const char* label, float* data, int N, int C, int H, int W) {
     std::cout << label << ":\n";
     for (int n = 0; n < N; ++n)
@@ -30,27 +29,28 @@ int main() {
     const int total = N * C * H * W;
     const float epsilon = 1e-5f;
 
-    float h_input[total] = {1, 2, 3, 4,  5, 6, 7, 8};
+    float h_input[total] = {1, 2, 3, 4, 5, 6, 7, 8};
     float h_gamma[C] = {1.0f, 1.0f};
-    float h_beta[C]  = {0.0f, 0.0f};
+    float h_beta[C] = {0.0f, 0.0f};
 
-    float *d_input, *d_mean, *d_var, *d_gamma, *d_beta;
+    float *d_input, *d_output, *d_mean, *d_var, *d_gamma, *d_beta;
     CHECK_CUDA(cudaMalloc(&d_input, total * sizeof(float)));
+    CHECK_CUDA(cudaMalloc(&d_output, total * sizeof(float)));
     CHECK_CUDA(cudaMalloc(&d_mean, C * sizeof(float)));
     CHECK_CUDA(cudaMalloc(&d_var, C * sizeof(float)));
     CHECK_CUDA(cudaMalloc(&d_gamma, C * sizeof(float)));
-    CHECK_CUDA(cudaMalloc(&d_beta,  C * sizeof(float)));
+    CHECK_CUDA(cudaMalloc(&d_beta, C * sizeof(float)));
 
     CHECK_CUDA(cudaMemcpy(d_input, h_input, total * sizeof(float), cudaMemcpyHostToDevice));
     CHECK_CUDA(cudaMemcpy(d_gamma, h_gamma, C * sizeof(float), cudaMemcpyHostToDevice));
-    CHECK_CUDA(cudaMemcpy(d_beta,  h_beta,  C * sizeof(float), cudaMemcpyHostToDevice));
+    CHECK_CUDA(cudaMemcpy(d_beta, h_beta, C * sizeof(float), cudaMemcpyHostToDevice));
 
-    batchnorm_forward(d_input, d_mean, d_var, d_gamma, d_beta, N, C, H, W, epsilon);
+    batchnorm_forward(d_input, d_output, d_mean, d_var, d_gamma, d_beta, N, C, H, W, epsilon);
 
     float h_output[total], h_mean[C], h_var[C];
-    CHECK_CUDA(cudaMemcpy(h_output, d_input, total * sizeof(float), cudaMemcpyDeviceToHost));
+    CHECK_CUDA(cudaMemcpy(h_output, d_output, total * sizeof(float), cudaMemcpyDeviceToHost));
     CHECK_CUDA(cudaMemcpy(h_mean, d_mean, C * sizeof(float), cudaMemcpyDeviceToHost));
-    CHECK_CUDA(cudaMemcpy(h_var,  d_var,  C * sizeof(float), cudaMemcpyDeviceToHost));
+    CHECK_CUDA(cudaMemcpy(h_var, d_var, C * sizeof(float), cudaMemcpyDeviceToHost));
 
     print_tensor("Input Normalized", h_output, N, C, H, W);
 
@@ -60,8 +60,7 @@ int main() {
     for (int i = 0; i < C; ++i) std::cout << h_var[i] << " ";
     std::cout << std::endl;
 
-    // Cleanup
-    cudaFree(d_input); cudaFree(d_mean); cudaFree(d_var);
-    cudaFree(d_gamma); cudaFree(d_beta);
+    cudaFree(d_input); cudaFree(d_output); cudaFree(d_mean);
+    cudaFree(d_var); cudaFree(d_gamma); cudaFree(d_beta);
     return 0;
 }
